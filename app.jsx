2401fdcfe929c9bdfa6c4813f5c01dcee3b1a323
Santiago <santiago_1016@hotmail.com>
2017-12-27 23:46:43 -0500

    separando los componentes

--- app.jsx
+++ app.jsx
@@ -1,35 +1,67 @@
+function Header(props){
+	return(
+		<div className = "header">
+				<h1>{props.title} </h1>
+		</div>
+		);
+}
+
+function Player(props){
+	return(
+		<div className = "player">
+			<div className = "player-name">
+				{props.name}
+			</div>
+			<div className = "player-score">
+				<Counter score= {props.score} />
+			</div>	
+		</div>
+		);
+};
+
+function Counter(props){
+	return(
+		<div className = "counter">
+			<button className = "counter-action decrement">-</button>
+			<div className = "counter-score">
+				{props.score}
+			</div>
+			<button className = "counter-action increment">+</button>
+		</div>
+		);
+}
+
+
 function Application(props) {
 	return(
 		<div className = "scoreboard ">
-			<div className = "header">
-				<h1>{props.title} </h1>
-			</div>
+			<Header title={props.title}/> {/*agregando un componente dentro de otro*/}
 			<div className = "players">
-				<div className = "player">
-					<div className = "player-name">
-						holi
-					</div>
-					<div className = "player-score">
-						<div className = "counter">
-							<button className = "counter-action decrement">-</button>
-							<div className = "counter-score">31	</div>
-							<button className = "counter-action increment">+</button>
-						</div>
-					</div>
-				</div>
+				<Player name ={props.name}/>
 			</div>
 		</div>
 	);
 }
 
 //Configurando la propiedades
-Application.propTypes = {
-	title: React.PropTypes.string
+Header.propTypes = {
+	title: React.PropTypes.string.isRequired
 };
 
+Player.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	score: React.PropTypes.number.isRequired
+};
+
+
 //Asociando valores a las propiedades
-Application.defaulProps = {
+Header.defaultProps = {
 	title: "Score Board"
+};
+
+Player.defaultProps = {
+	name: "Santiago Bedoya",
+	score : 3
 };
 
 
