09db9cb0fb1ae2a00f2e414595c5abe8fbde60b2
Santiago <santiago_1016@hotmail.com>
2017-12-28 00:11:54 -0500

    agregando arreglos

--- app.jsx
+++ app.jsx
@@ -1,3 +1,16 @@
+var PLAYERS = [
+	{
+		name: "Santiago",
+		score: 3,
+		id: 1 //key props
+	},
+	{
+		name: "Bedoya",
+		score: 32,
+		id: 2
+	}
+]
+
 function Header(props){
 	return(
 		<div className = "header">
@@ -37,7 +50,9 @@ function Application(props) {
 		<div className = "scoreboard ">
 			<Header title={props.title}/> {/*agregando un componente dentro de otro*/}
 			<div className = "players">
-				<Player name ={props.name}/>
+			{props.players.map(function(player){
+				return <Player name={player.name} score={player.score} key={player.id}/>
+			})}
 			</div>
 		</div>
 	);
@@ -50,19 +65,24 @@ Header.propTypes = {
 
 Player.propTypes = {
 	name: React.PropTypes.string.isRequired,
-	score: React.PropTypes.number.isRequired
 };
+
+Counter.propTypes = {
+	score: React.PropTypes.number.isRequired
+}
+Application.propTypes ={
+	players: React.PropTypes.arrayOf(React.PropTypes.shape({
+			name: React.PropTypes.string.isRequired,
+			score: React.PropTypes.number.isRequired,
+			id: React.PropTypes.number.isRequired
+		})).isRequired
+}
 
 
 //Asociando valores a las propiedades
-Header.defaultProps = {
+Application.defaultProps = {
 	title: "Score Board"
 };
 
-Player.defaultProps = {
-	name: "Santiago Bedoya",
-	score : 3
-};
 
-
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(<Application players={PLAYERS} />, document.getElementById('container'));
