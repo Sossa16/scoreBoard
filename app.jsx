4ce0f5124e307402d65dfe6092a1db3e789b5001
Santiago <santiago_1016@hotmail.com>
2017-12-30 19:49:58 -0500

    comunincado eventos con un erro de metodo

--- app.jsx
+++ app.jsx
@@ -14,18 +14,21 @@ var PLAYERS = [
 function Counter(props){
 	return(
 		<div className = "counter">
-			<button className = "counter-action decrement">-</button>
+			<button className = "counter-action decrement" onClick= {function(){props.onChange(-1);}}>-</button>
 			<div className = "counter-score">
 				{props.score} {/*Refencia en componente de clase*/}
 			</div>
-			<button className = "counter-action increment">+</button>
+			<button className = "counter-action increment" onClick= {function(){props.onChange(+1);}}>+</button>
 		</div>
 	);
 }
 
-Counter.PropTypes={
-	Score: React.PropTypes.number.isRequired
+Counter.PropTypes = {
+	Score: React.PropTypes.number.isRequired,
+	onChange: React.PropTypes.func.isRequired,
+	onScoreChange: React.PropTypes.func.isRequired
 }
+
 function Header(props){
 	return(
 		<div className = "header">
@@ -34,6 +37,10 @@ function Header(props){
 		);
 }
 
+Header.propTypes = {
+	title: React.PropTypes.string.isRequired
+};
+
 function Player(props){
 	return(
 		<div className = "player">
@@ -41,11 +48,16 @@ function Player(props){
 				{props.name}
 			</div>
 			<div className = "player-score">
-				<Counter score = {props.score}/>
+				<Counter score = {props.score} onClick={props.onScoreChange}/>
 			</div>	
 		</div>
 		);
 };
+
+Player.propTypes = {
+	name: React.PropTypes.string.isRequired,
+};
+
 
 var Application = React.createClass({
 
@@ -68,7 +80,11 @@ var Application = React.createClass({
 	getInitialState: function() {
 		return{ 
 			players: this.props.initialPlayers
-		};i
+		};
+	},
+
+	onScoreChange: function(delta){
+		console.log('onScoreChange', delta)
 	},
 
 	render: function(){
@@ -77,23 +93,18 @@ var Application = React.createClass({
 				<Header title={this.props.title}/> {/*agregando un componente dentro de otro*/}
 				<div className = "players">
 					{this.state.players.map(function(player){
-						return <Player name={player.name} score={player.score} key={player.id}/>
-					})}
+						return (
+							<Player 
+							onScoreChange={function(delta){this.onScoreChange(delta).bind(this)}}
+							name={player.name}
+							score={player.score}
+							key={player.id}/>
+							);
+					}.bind(this))}
 				</div>
 			</div>
 		);
 	}
 });
-
-
-//Configurando la propiedades
-Header.propTypes = {
-	title: React.PropTypes.string.isRequired
-};
-
-Player.propTypes = {
-	name: React.PropTypes.string.isRequired,
-};
-
 
 ReactDOM.render(<Application initialPlayers={PLAYERS} />, document.getElementById('container'));
